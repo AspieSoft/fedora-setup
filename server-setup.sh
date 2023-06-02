f7b49df3a04cff00b7c94046bbd23d6a822c2ea4
AspieSoft <aspiesoftweb@gmail.com>
2023-06-02 09:46:20 -0400

    updated

---
+++ server-setup.sh
@@ -0,0 +1,116 @@
+#!/bin/bash
+
+if ! [ "$1" = "-y" ]; then
+  echo
+  echo "This is the install for web servers."
+  echo "If your looking for a gnome desktop setup, please run setup.sh instead."
+  echo "This scripts installs some of the basics for a web server (optimized for linode)"
+  echo
+  read -n1 -p "Would you like to continue with the install (Y/n)? " input ; echo >&2
+
+  if ! [ "$input" = "y" -o "$input" = "Y" -o "$input" = "" -o "$input" = " " ] ; then
+    echo "install canceled!"
+    exit
+  fi
+fi
+
+# improve dnf speed
+echo "#Added for Speed" | sudo tee -a /etc/dnf/dnf.conf
+echo "fastestmirror=True" | sudo tee -a /etc/dnf/dnf.conf
+echo "max_parallel_downloads=5" | sudo tee -a /etc/dnf/dnf.conf
+echo "defaultyes=True" | sudo tee -a /etc/dnf/dnf.conf
+echo "keepcache=True" | sudo tee -a /etc/dnf/dnf.conf
+
+sudo dnf -y update
+sudo dnf -y makecache
+
+# install python, c++, and java
+sudo dnf -y install python python3 python-pip
+sudo dnf -y install gcc-c++ make gcc
+sudo dnf -y install java-1.8.0-openjdk.x86_64
+sudo dnf -y install java-11-openjdk.x86_64
+sudo dnf -y install java-latest-openjdk.x86_64
+
+# install nodejs
+sudo dnf -y install nodejs
+sudo npm -g i npm
+npm config set prefix ~/.npm
+
+# add npm for new user
+if ! [ -f "/etc/skel/.zshrc" ]; then
+  sudo touch "/etc/skel/.zshrc"
+fi
+if ! [ -f "/etc/skel/.profile" ]; then
+  sudo touch "/etc/skel/.profile"
+fi
+if ! grep -q 'export N_PREFIX="~/.npm"' "/etc/skel/.zshrc" ; then
+  echo 'export N_PREFIX="~/.npm"' | sudo tee -a "/etc/skel/.zshrc"
+fi
+if ! grep -q 'export N_PREFIX="~/.npm"' "/etc/skel/.profile" ; then
+  echo 'export N_PREFIX="~/.npm"' | sudo tee -a "/etc/skel/.profile"
+fi
+
+# install yarn, and git
+sudo npm -g i yarn
+sudo dnf -y install git
+
+# install golang
+sudo dnf -y install golang
+
+# install clamav
+sudo dnf -y install clamav clamd clamav-update
+sudo systemctl stop clamav-freshclam
+sudo freshclam
+sudo systemctl enable clamav-freshclam --now
+
+sudo dnf -y install cronie
+
+sudo freshclam
+
+# add quarantine folder
+if ! [ -d "/VirusScan/quarantine" ]; then
+  sudo mkdir -p /VirusScan/quarantine
+  sudo chmod 0664 /VirusScan
+  sudo chmod 2660 /VirusScan/quarantine
+  sudo chmod -R 2660 /VirusScan/quarantine
+fi
+
+# fix clamav permissions
+if grep -R "^ScanOnAccess " "/etc/clamd.d/scan.conf"; then
+  sudo sed -r -i 's/^ScanOnAccess (.*)$/ScanOnAccess yes/m' "/etc/clamd.d/scan.conf"
+else
+  echo 'ScanOnAccess yes' | sudo tee -a "/etc/clamd.d/scan.conf"
+fi
+
+if grep -R "^OnAccessMountPath " "/etc/clamd.d/scan.conf"; then
+  sudo sed -r -i 's#^OnAccessMountPath (.*)$#OnAccessMountPath /#m' "/etc/clamd.d/scan.conf"
+else
+  echo 'OnAccessMountPath /' | sudo tee -a "/etc/clamd.d/scan.conf"
+fi
+
+if grep -R "^OnAccessPrevention " "/etc/clamd.d/scan.conf"; then
+  sudo sed -r -i 's/^OnAccessPrevention (.*)$/OnAccessPrevention no/m' "/etc/clamd.d/scan.conf"
+else
+  echo 'OnAccessPrevention no' | sudo tee -a "/etc/clamd.d/scan.conf"
+fi
+
+if grep -R "^OnAccessExtraScanning " "/etc/clamd.d/scan.conf"; then
+  sudo sed -r -i 's/^OnAccessExtraScanning (.*)$/OnAccessExtraScanning yes/m' "/etc/clamd.d/scan.conf"
+else
+  echo 'OnAccessExtraScanning yes' | sudo tee -a "/etc/clamd.d/scan.conf"
+fi
+
+if grep -R "^OnAccessExcludeUID " "/etc/clamd.d/scan.conf"; then
+  sudo sed -r -i 's/^OnAccessExcludeUID (.*)$/OnAccessExcludeUID 0/m' "/etc/clamd.d/scan.conf"
+else
+  echo 'OnAccessExcludeUID 0' | sudo tee -a "/etc/clamd.d/scan.conf"
+fi
+
+if grep -R "^User " "/etc/clamd.d/scan.conf"; then
+  sudo sed -r -i 's/^User (.*)$/User root/m' "/etc/clamd.d/scan.conf"
+else
+  echo 'User root' | sudo tee -a "/etc/clamd.d/scan.conf"
+fi
+
+sudo dnf -y update
+sudo dnf clean all
