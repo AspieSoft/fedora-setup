76023a4608a189ac74ff4dc2ee0ab1aa0d3e6fe3
AspieSoft <aspiesoftweb@gmail.com>
2023-06-01 20:14:05 -0400

    updated

--- assets/apps/aspiesoft-fedora-setup-aliases.sh
+++ assets/apps/aspiesoft-fedora-setup-aliases.sh
@@ -9,7 +9,7 @@ function update() {
 function avscan() {
   local scanDir="$1"
   if [ "$scanDir" = "" ]; then
-    scanDir="/"
+    scanDir="$HOME"
   fi
   sudo nice -n 15 clamscan && sudo clamscan -r --bell --move="/VirusScan/quarantine" --exclude-dir="/VirusScan/quarantine" --exclude-dir="/home/$USER/.clamtk/viruses" --exclude-dir="smb4k" --exclude-dir="/run/user/$USER/gvfs" --exclude-dir="/home/$USER/.gvfs" --exclude-dir=".thunderbird" --exclude-dir=".mozilla-thunderbird" --exclude-dir=".evolution" --exclude-dir="Mail" --exclude-dir="kmail" --exclude-dir="^/sys" "$scanDir"
   unset scanDir
--- bin/updates/0.0.1.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# This is the first version
-# Nothing to do here, just wanted github to keep my updates folder
