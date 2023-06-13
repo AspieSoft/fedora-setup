303c43e8f46945f347f4e32b2eafd50d571b6649
AspieSoft <aspiesoftweb@gmail.com>
2023-06-12 23:10:03 -0400

    updated

--- bin/updates/0.1.0.sh
+++ bin/updates/0.1.0.sh
@@ -6,8 +6,8 @@ if ! [ -d "/etc/aspiesoft-clamav-scanner" ]; then
   git clone https://github.com/AspieSoft/linux-clamav-download-scanner.git
   sudo -R -f cp linux-clamav-download-scanner/* /etc/aspiesoft-clamav-scanner
   rm linux-clamav-download-scanner
-  sudo cp -f ./assets/apps/aspiesoft-clamav-scanner/start.sh /etc/aspiesoft-clamav-scanner
-  sudo cp -f ./assets/apps/aspiesoft-clamav-scanner/aspiesoft-clamav-scanner.desktop "$HOME/.config/autostart"
+  sudo cp -f ../../assets/apps/aspiesoft-clamav-scanner/start.sh /etc/aspiesoft-clamav-scanner
+  sudo cp -f ../../assets/apps/aspiesoft-clamav-scanner/aspiesoft-clamav-scanner.desktop "$HOME/.config/autostart"
 fi
 
 notify-send -t 10 "Notice: the auto update script has been updated on github, and cannot be auto updated" "If you were using beta 0.0.5 or later, please manually download the new auto update script form 'https://github.com/AspieSoft/fedora-setup/tree/0.1.0/assets/apps/aspiesoft-fedora-setup-updates' and replace the script in your local directory at '/etc/aspiesoft-fedora-setup-updates'"
