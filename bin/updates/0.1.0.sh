0db10b7b9055b9640f5453fc2c6acf4269c04a33
AspieSoft <aspiesoftweb@gmail.com>
2023-06-12 23:13:13 -0400

    updated

--- bin/updates/0.1.0.sh
+++ bin/updates/0.1.0.sh
@@ -16,4 +16,4 @@ echo "Notice: the auto update script has been updated on github, and cannot be a
 echo "" >> "$HOME/Desktop/aspiesoft-manual-update-notice.txt"
 echo "If you were using beta 0.0.5 or later, please manually download the new auto update script form 'https://github.com/AspieSoft/fedora-setup/tree/0.1.0/assets/apps/aspiesoft-fedora-setup-updates' and replace the script in your local directory at '/etc/aspiesoft-fedora-setup-updates'" >> "$HOME/Desktop/aspiesoft-manual-update-notice.txt"
 echo "" >> "$HOME/Desktop/aspiesoft-manual-update-notice.txt"
-echo "This kind of update should not be as common once the script is out of beta (1.0.0)" >> "$HOME/Desktop/aspiesoft-manual-update-notice.txt"
+echo "This kind of update should not be as common once the script is out of beta" >> "$HOME/Desktop/aspiesoft-manual-update-notice.txt"
