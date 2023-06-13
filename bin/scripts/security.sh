1fe3efe21e4d0a40b314c4c495ea524d27027304
AspieSoft <aspiesoftweb@gmail.com>
2023-06-12 22:44:26 -0400

    updated

--- bin/scripts/security.sh
+++ bin/scripts/security.sh
@@ -56,6 +56,18 @@ else
   echo 'User root' | sudo tee -a "/etc/clamd.d/scan.conf"
 fi
 
+
+# install aspiesoft clamav download scanner
+if ! [ -d "/etc/aspiesoft-clamav-scanner" ]; then
+  sudo mkdir -p /etc/aspiesoft-clamav-scanner
+  git clone https://github.com/AspieSoft/linux-clamav-download-scanner.git
+  sudo -R -f cp linux-clamav-download-scanner/* /etc/aspiesoft-clamav-scanner
+  rm linux-clamav-download-scanner
+  sudo cp -f ./assets/apps/aspiesoft-clamav-scanner/start.sh /etc/aspiesoft-clamav-scanner
+  sudo cp -f ./assets/apps/aspiesoft-clamav-scanner/aspiesoft-clamav-scanner.desktop "$HOME/.config/autostart"
+fi
+
+
 # install bleachbit
 sudo dnf -y install bleachbit
 
