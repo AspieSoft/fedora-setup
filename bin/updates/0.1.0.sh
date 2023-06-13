c902ca58d6e154332d7aa3bd0d0890a58c60efc3
AspieSoft <aspiesoftweb@gmail.com>
2023-06-12 23:05:51 -0400

    updated

--- bin/updates/0.1.0.sh
+++ bin/updates/0.1.0.sh
@@ -9,3 +9,11 @@ if ! [ -d "/etc/aspiesoft-clamav-scanner" ]; then
   sudo cp -f ./assets/apps/aspiesoft-clamav-scanner/start.sh /etc/aspiesoft-clamav-scanner
   sudo cp -f ./assets/apps/aspiesoft-clamav-scanner/aspiesoft-clamav-scanner.desktop "$HOME/.config/autostart"
 fi
+
+notify-send -t 10 "Notice: the auto update script has been updated on github, and cannot be auto updated" "If you were using beta 0.0.5 or later, please manually download the new auto update script form 'https://github.com/AspieSoft/fedora-setup/tree/0.1.0/assets/apps/aspiesoft-fedora-setup-updates' and replace the script in your local directory at '/etc/aspiesoft-fedora-setup-updates'"
+
+echo "Notice: the auto update script has been updated on github, and cannot be auto updated" >> "$HOME/Desktop/aspiesoft-manual-update-notice.txt"
+echo "" >> "$HOME/Desktop/aspiesoft-manual-update-notice.txt"
+echo "If you were using beta 0.0.5 or later, please manually download the new auto update script form 'https://github.com/AspieSoft/fedora-setup/tree/0.1.0/assets/apps/aspiesoft-fedora-setup-updates' and replace the script in your local directory at '/etc/aspiesoft-fedora-setup-updates'" >> "$HOME/Desktop/aspiesoft-manual-update-notice.txt"
+echo "" >> "$HOME/Desktop/aspiesoft-manual-update-notice.txt"
+echo "This kind of update should not be as common once the script is out of beta (1.0.0)" >> "$HOME/Desktop/aspiesoft-manual-update-notice.txt"
