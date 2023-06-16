900e90fcbee8b2e5401cc5a8e694d1b5117d5827
AspieSoft <aspiesoftweb@gmail.com>
2023-06-16 16:01:22 -0400

    added checkpoints to install steps allows install to pick up where it left off in some cases

--- setup.sh
+++ setup.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
 cd $(dirname "$0")
+dir="$PWD"
+
 
 autoUpdates="y"
 slowWifi="n"
+
+processStep="0"
+
+if [ -f "ProcessStep.txt" ]; then
+  processStep="$(cat ProcessStep.txt)"
+fi
 
 if ! [ "$1" = "-y" ]; then
   echo
@@ -41,8 +49,17 @@ function cleanup() {
 
   # reenable dnf timeout if temporarly disabled
   sudo perl -0777 -i -pe 's/#AspieSoft-TEMP-START(.*)#AspieSoft-TEMP-END//s' /etc/dnf/dnf.conf &>/dev/null
+
+  cd "$dir"
+  echo "$processStep" | sudo tee "ProcessStep.txt"
 }
 trap cleanup EXIT
+
+function cleanupexit() {
+  cleanup
+  exit
+}
+trap cleanupexit SIGINT
 
 
 # To log into sudo with password prompt
@@ -131,81 +148,137 @@ else
   sudo perl -0777 -i -pe 's/#AspieSoft-TEMP-START(.*)#AspieSoft-TEMP-END//s' /etc/dnf/dnf.conf &>/dev/null
 fi
 
-echo "updating..."
-waitForWifi sudo dnf -y update
+if [ "$processStep" -lt "1" ]; then
+  echo "updating..."
+  waitForWifi sudo dnf -y update
+
+  processStep=$((processStep+1))
+fi
 
 # install ufw and disable firewalld
-echo "installing ufw..."
-waitForWifi sudo dnf -y install ufw
-sudo systemctl stop firewalld
-sudo systemctl disable firewalld
-sudo systemctl enable ufw
-sudo systemctl start ufw
-sudo ufw enable
-sudo ufw delete allow SSH
-sudo ufw delete allow to 244.0.0.251 app mDNS
-sudo ufw delete allow to ff02::fb app mDNS
+if [ "$processStep" -lt "2" ]; then
+  echo "installing ufw..."
+  waitForWifi sudo dnf -y install ufw
+  sudo systemctl stop firewalld
+  sudo systemctl disable firewalld
+  sudo systemctl enable ufw
+  sudo systemctl start ufw
+  sudo ufw enable
+  sudo ufw delete allow SSH
+  sudo ufw delete allow to 244.0.0.251 app mDNS
+  sudo ufw delete allow to ff02::fb app mDNS
 
-waitForWifi sudo dnf -y makecache
+  processStep=$((processStep+1))
+fi
+
+if [ "$processStep" -lt "3" ]; then
+  waitForWifi sudo dnf -y makecache
+
+  processStep=$((processStep+1))
+fi
 
 # RUN programing-languages.sh
-echo "installing programing languages..."
-bash "./bin/scripts/programing-languages.sh"
+if [ "$processStep" -lt "4" ]; then
+  echo "installing programing languages..."
+  bash "./bin/scripts/programing-languages.sh"
+
+  processStep=$((processStep+1))
+fi
 
 # update grub timeout
-echo "changing grub timeout..."
-sudo cp -n /etc/default/grub /etc/default/grub-backup
-sudo sed -r -i 's/^GRUB_TIMEOUT_STYLE=(.*)$/GRUB_TIMEOUT_STYLE=menu/m' /etc/default/grub
-sudo sed -r -i 's/^GRUB_TIMEOUT=(.*)$/GRUB_TIMEOUT=0/m' /etc/default/grub
-sudo update-grub
+if [ "$processStep" -lt "5" ]; then
+  echo "changing grub timeout..."
+  sudo cp -n /etc/default/grub /etc/default/grub-backup
+  sudo sed -r -i 's/^GRUB_TIMEOUT_STYLE=(.*)$/GRUB_TIMEOUT_STYLE=menu/m' /etc/default/grub
+  sudo sed -r -i 's/^GRUB_TIMEOUT=(.*)$/GRUB_TIMEOUT=0/m' /etc/default/grub
+  sudo update-grub
+
+  processStep=$((processStep+1))
+fi
 
 # RUN preformance.sh
-echo "installing preformance upgrades..."
-bash "./bin/scripts/preformance.sh"
+if [ "$processStep" -lt "6" ]; then
+  echo "installing preformance upgrades..."
+  bash "./bin/scripts/preformance.sh"
+
+  processStep=$((processStep+1))
+fi
 
 # RUN fix.sh
-echo "installing common fices..."
-bash "./bin/scripts/fix.sh"
+if [ "$processStep" -lt "7" ]; then
+  echo "installing common fices..."
+  bash "./bin/scripts/fix.sh"
+
+  processStep=$((processStep+1))
+fi
 
 # RUN security.sh
-echo "installing security apps..."
-bash "./bin/scripts/security.sh"
+if [ "$processStep" -lt "8" ]; then
+  echo "installing security apps..."
+  bash "./bin/scripts/security.sh"
+
+  processStep=$((processStep+1))
+fi
 
 # RUN repos.sh
-echo "installing repos..."
-bash "./bin/scripts/repos.sh"
+if [ "$processStep" -lt "9" ]; then
+  echo "installing repos..."
+  bash "./bin/scripts/repos.sh"
+
+  processStep=$((processStep+1))
+fi
 
 # RUN apps.sh
-echo "installing apps..."
-bash "./bin/scripts/apps.sh"
+if [ "$processStep" -lt "10" ]; then
+  echo "installing apps..."
+  bash "./bin/scripts/apps.sh"
+
+  processStep=$((processStep+1))
+fi
 
 # RUN shortcuts.sh
-echo "installing shortcuts..."
-bash "./bin/scripts/shortcuts.sh"
+if [ "$processStep" -lt "11" ]; then
+  echo "installing shortcuts..."
+  bash "./bin/scripts/shortcuts.sh"
 
-waitForWifi sudo dnf -y update
-echo "running dnf clean..."
-sudo dnf clean all
+  processStep=$((processStep+1))
+fi
+
+if [ "$processStep" -lt "12" ]; then
+  waitForWifi sudo dnf -y update
+  echo "running dnf clean..."
+  sudo dnf clean all
+
+  processStep=$((processStep+1))
+fi
 
 # RUN theme.sh
-echo "installing theme..."
-bash "./bin/scripts/theme.sh"
+if [ "$processStep" -lt "13" ]; then
+  echo "installing theme..."
+  bash "./bin/scripts/theme.sh"
+
+  processStep=$((processStep+1))
+fi
 
 
 # setup aspiesoft auto updates
 if [ "$autoUpdates" = "y" -o "$autoUpdates" = "Y" -o "$autoUpdates" = "" -o "$autoUpdates" = " " ] ; then
-  echo "installing auto updates..."
+  if [ "$processStep" -lt "14" ]; then
+    echo "installing auto updates..."
 
-  sudo mkdir -p /etc/aspiesoft-fedora-setup-updates
-  sudo cp -rf ./assets/apps/aspiesoft-fedora-setup-updates/* /etc/aspiesoft-fedora-setup-updates
-  sudo rm -f /etc/aspiesoft-fedora-setup-updates/aspiesoft-fedora-setup-updates.service
-  sudo cp -f ./assets/apps/aspiesoft-fedora-setup-updates/aspiesoft-fedora-setup-updates.service "/etc/systemd/system"
-  gitVer="$(waitForWifi curl --silent 'https://api.github.com/repos/AspieSoft/fedora-setup/releases/latest' | grep '\"tag_name\":' | sed -E 's/.*\"([^\"]+)\".*/\1/')"
-  echo "$gitVer" | sudo tee "/etc/aspiesoft-fedora-setup-updates/version.txt"
+    sudo mkdir -p /etc/aspiesoft-fedora-setup-updates
+    sudo cp -rf ./assets/apps/aspiesoft-fedora-setup-updates/* /etc/aspiesoft-fedora-setup-updates
+    sudo rm -f /etc/aspiesoft-fedora-setup-updates/aspiesoft-fedora-setup-updates.service
+    sudo cp -f ./assets/apps/aspiesoft-fedora-setup-updates/aspiesoft-fedora-setup-updates.service "/etc/systemd/system"
+    gitVer="$(waitForWifi curl --silent 'https://api.github.com/repos/AspieSoft/fedora-setup/releases/latest' | grep '\"tag_name\":' | sed -E 's/.*\"([^\"]+)\".*/\1/')"
+    echo "$gitVer" | sudo tee "/etc/aspiesoft-fedora-setup-updates/version.txt"
 
-  sudo systemctl daemon-reload
-  sudo systemctl enable aspiesoft-fedora-setup-updates.service
-  sudo systemctl start aspiesoft-fedora-setup-updates.service
+    sudo systemctl daemon-reload
+    sudo systemctl enable aspiesoft-fedora-setup-updates.service
+    sudo systemctl start aspiesoft-fedora-setup-updates.service
+
+    processStep=$((processStep+1))
+  fi
 fi
 
 
