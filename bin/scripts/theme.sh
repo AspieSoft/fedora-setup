37d7a8c8a6fff93cc116baecbe75e347befb8fb0
AspieSoft <aspiesoftweb@gmail.com>
2023-06-17 00:31:30 -0400

    updated

--- bin/scripts/theme.sh
+++ bin/scripts/theme.sh
@@ -80,10 +80,20 @@ dconf reset /org/gnome/desktop/wm/keybindings/move-to-monitor-down
 dconf reset /org/gnome/desktop/wm/keybindings/move-to-monitor-left
 dconf reset /org/gnome/desktop/wm/keybindings/move-to-monitor-right
 
+dconf reset /org/gnome/desktop/wm/keybindings/maximize
+dconf reset /org/gnome/desktop/wm/keybindings/unmaximize
+dconf reset /org/gnome/desktop/wm/keybindings/toggle-tiled-left
+dconf reset /org/gnome/desktop/wm/keybindings/toggle-tiled-left
+
 dconf write /org/gnome/desktop/wm/keybindings/move-to-monitor-up "['']"
 dconf write /org/gnome/desktop/wm/keybindings/move-to-monitor-down "['']"
 dconf write /org/gnome/desktop/wm/keybindings/move-to-monitor-left "['']"
 dconf write /org/gnome/desktop/wm/keybindings/move-to-monitor-right "['']"
+
+dconf write /org/gnome/desktop/wm/keybindings/maximize "['']"
+dconf write /org/gnome/desktop/wm/keybindings/unmaximize "['']"
+dconf write /org/gnome/desktop/wm/keybindings/toggle-tiled-left "['']"
+dconf write /org/gnome/desktop/wm/keybindings/toggle-tiled-left "['']"
 
 dconf write /org/gnome/desktop/wm/keybindings/switch-to-workspace-up "['<Super>Up']"
 dconf write /org/gnome/desktop/wm/keybindings/switch-to-workspace-down "['<Super>Down']"
